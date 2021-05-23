bbd8c0aa8e6ac9e421ea91f85d1041ff49247458
Jamee Hengari <jhengari@hotmail.com>
2021-05-23 19:05:11 -0400

    bulk of the work done together commit

---
+++ youtube-react-app/src/Components/Videos.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import YouTube from "react-youtube";
+
+export class Videos extends Component {
+  render() {
+    const opts = {
+      height: "490",
+      width: "740",
+      playerVars: {
+        autoplay: 1,
+      },
+    };
+    return (
+      <div>
+        <YouTube videoId={this.props.match.params.id} opts={opts} />
+      </div>
+    );
+  }
+}
+
+export default Videos;
